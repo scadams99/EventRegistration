f8700ebdd860832b2cdda450ab8e7dee8c246b95
scadams99 <scadams@earthlink.net>
2013-10-05 10:00:33 -0400

    Finished CH 25
    Layouts, partial views and jQuery intellisense

---
+++ EventRegistration/Infrastructure/ExternalHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+namespace EventRegistration.Infrastructure
+{
+    public static class ExternalHelpers
+    {
+        public static string RegistrationCount(this HtmlHelper html, int i)
+        {
+            string result;
+            switch (i)
+            {
+                case 0:
+                    result = "There are no registrations";
+                    break;
+                case 1:
+                    result = "There is one registration";
+                    break;
+                default:
+                    result = string.Format("There are {0} registrations", i);
+                    break;
+            }
+            return result;
+        }
+    }
+}
