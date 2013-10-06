3d1a7e256c68bb04d4b3b905b4d27a5d7d4acc22
scadams99 <scadams@earthlink.net>
2013-10-06 10:07:37 -0400

    Finishes Ch26 with custom helpers

---
+++ EventRegistration/Controllers/CompetitionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using EventRegistration.Models.View;
+namespace EventRegistration.Controllers
+{
+    public class CompetitionController : Controller
+    {
+        public ActionResult Summary()
+        {
+            CompetitionSummary summary = new CompetitionSummary
+            {
+                Name = "Mass Mangler",
+                City = "Boston",
+                Date = new System.DateTime(2013, 1, 20),
+                Approved = true,
+                Start = StartTime.Evening
+            };
+            return View(summary);
+        }
+    }
+}
---
+++ EventRegistration/Models/View/CompetitionSummary.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace EventRegistration.Models.View
+{
+    public enum StartTime
+    {
+        Morning,
+        Midday,
+        Evening
+    }
+
+    public class CompetitionSummary
+    {
+        [DisplayName("Event Name")]
+        public string Name { get; set; }
+        
+        [DisplayName("Location")]
+        [UIHint("City")]
+        public string City { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [HiddenInput(DisplayValue = true)]
+        public bool Approved { get; set; }
+
+        public StartTime Start { get; set; }
+    }
+}
